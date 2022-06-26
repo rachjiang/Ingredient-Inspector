3650a11977476a4eb747618e49a79d9282044ce2
rachelj521 <rjiang521@gmail.com>
2022-06-26 00:08:52 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+// const predictRouter = require('./routes/predict');
+const http = require('http');
+
+const app = express();
+// urlencoded recognizes the incoming request obj as strings or arrays; json recognizes it as a JSON obj
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/hello', function (req, res) {
+    res.send('hello')
+})
+// app.use('/predict', predictRouter)
+
+const port = process.env.PORT || '8080';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
+server.on('listening', () => {
+    console.log('Listening on ' + port)
+});
